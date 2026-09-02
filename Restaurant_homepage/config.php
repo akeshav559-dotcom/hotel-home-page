d2ed90327e6e2a5b4c139f160eaf85e80b0630d5
akeshav559-dotcom <akeshav559@gmail.com>
2026-09-03 00:34:25 +0530

    Add files via upload

---
+++ Restaurant_homepage/config.php
@@ -0,0 +1,18 @@
+```php id="v0s2kn"
+<?php
+
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "Restaurant_homepage"
+);
+
+if(!$conn){
+
+    die("Database Connection Failed");
+
+}
+
+?>
+```
